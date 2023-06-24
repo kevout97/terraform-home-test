aa88704400cd676acf24e04e0294b2410b42ca1a
kevout97 <kev.gomez97@hotmail.com>
2023-06-24 11:43:36 -0600

    chroe: add route53

---
+++ route53/variables.tf
@@ -0,0 +1,19 @@
+variable "aws_region" {
+  type        = string
+  description = "AWS Region"
+}
+
+variable "aws_profile" {
+  type        = string
+  description = "AWS Profile"
+}
+
+variable "dns_domain" {
+  type        = string
+  description = "DNS domain"
+}
+
+variable "environment" {
+  type        = string
+  description = "Environment"
+}
